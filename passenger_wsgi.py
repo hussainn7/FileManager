6422fcbb11d0cfbb2589c04f6909f39b0356094b
Hussain <syedh2971@gmail.com>
2025-02-14 01:02:59 -0500

    Initial commit: File Exchange CMS

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 import sys, os
 
-# Add project directory to the sys.path
-sys.path.insert(0, '/home/u/undercover/getecu')
-sys.path.insert(1, '/home/u/undercover/.local/lib/python3.6/site-packages')
+# Add the project directory to the Python path
+path = os.path.dirname(os.path.abspath(__file__))
+if path not in sys.path:
+    sys.path.append(path)
 
-from app import app as application
+from app import app as application  # This is the Flask application object
+from werkzeug.debug import DebuggedApplication  # Опционально: подключение модуля отладки
 
-# Disable debug mode in production
+# Опционально: включение модуля отадки
+application.wsgi_app = DebuggedApplication(application.wsgi_app, True)
+
+# Отключаем режим отладки в production
 application.debug = False 
+
+# Optional: Set environment to production
+os.environ['FLASK_ENV'] = 'production'
+os.environ['FLASK_DEBUG'] = '0'
+
+# -*- coding: utf-8 -*-
+import sys, os
+sys.path.append('/home/u/undercover/getecu/') # указываем директорию с проектом
+sys.path.append('/home/d/deniatest/.local/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
+from HelloFlask import app as application # когда Flask стартует, он ищет application. Если не указать 'as application', сайт не заработает
+from werkzeug.debug import DebuggedApplication # Опционально: подключение модуля отладки
+application.wsgi_app = DebuggedApplication(application.wsgi_app, True) # Опционально: включение модуля отадки
+application.debug = False  # Опционально: True/False устанавливается по необходимости в отладке
